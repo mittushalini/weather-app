10ff2f4ea38f9d506e775fe254e499085811166b
Shalini <bhattacharyashalinimittu2@gmail.com>
2022-05-30 19:02:54 +0530

    Init commit

---
+++ app.js
@@ -0,0 +1,96 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const temp = require('../functions/temp.js')
+const geocode = require('../functions/geocode.js')
+const { response } = require('express')
+
+const app = express()
+const PublicDirPath = path.join(__dirname, '/public')
+const ViewsDirPath = path.join(__dirname, '/public/views')
+const PartialsDirPath = path.join(__dirname, '/public/partials')
+
+app.use(express.static(PublicDirPath))
+app.set('view engine', 'hbs')
+app.set('views', ViewsDirPath)
+hbs.registerPartials(PartialsDirPath)
+
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'CheckWeatherHere!',
+        name: 'Shalini'
+
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help Needed?',
+        description: 'Check if you have entered valid country name',
+        name: 'Shalini'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Page',
+        status: 'This is a weather checking page.',
+        name: 'Shalini'
+    })
+})
+
+//app.get('/weather', (req, res) => {
+//    res.render('weather', {
+//        title: 'Weather Checking',
+//        status: 'Hot',
+//        name: 'Shalini Bhattacharya'
+//    })
+//})
+
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send('Please provide an address')
+    }
+    geocode(req.query.address, (error, response) => {
+        if (error) {
+            res.send({ error: error })
+
+        }
+        else {
+            temp(response.latitude, response.longitude, (error, forecastData) => {
+                if (error) {
+                    res.send({ error: error })
+                }
+                else {
+                    res.send({
+                        latitude: response.latitude,
+                        longitude: response.longitude,
+                        location: response.location,
+                        temperature: forecastData
+
+                    })
+
+
+                }
+            })
+        }
+    })
+})
+
+app.get('/pp', (req, res) => {
+    res.render('pp', {
+        title: 'Privacy Policy',
+        name: 'Shalini'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: 'ERROR:404',
+
+    })
+})
+app.listen(3000, () => {
+    console.log('server is up on 3000')
+})
---
+++ public/js/back-app.js
@@ -0,0 +1,40 @@
+console.log('client side running')
+
+//fetch('http://localhost:3000/weather?address=Boston').then((response) => {
+//    response.json().then((data) => {
+//       console.log(data)
+//    })
+
+//})
+
+const form = document.querySelector('form')
+const search = document.querySelector('input')
+const message1 = document.querySelector('#m1')
+const message2 = document.querySelector('#m2')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+    if (!location) {
+        message1.textContent = 'Please provide an address'
+        message2.textContent = ''
+    } else {
+        message1.textContent = 'loading...'
+        message2.textContent = ''
+        fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+            response.json().then((data) => {
+                if (data.error) {
+                    message1.textContent = data.error
+                    message2.textContent = ''
+                }
+                else {
+                    message1.textContent = 'Location - ' + data.location
+                    message2.textContent = 'It is ' + data.temperature + ' degrees celcius outside.'
+                }
+            })
+
+        })
+
+    }
+})
