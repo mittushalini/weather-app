2d1d539ffa5d57c82bdfb848d9fa9fb9a3fa5575
Shalini <bhattacharyashalinimittu2@gmail.com>
2022-05-30 22:39:45 +0530

    eliminating unnecessary path

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const hbs = require('hbs')
 const temp = require('./functions/temp.js')
 const geocode = require('./functions/geocode.js')
 const { response } = require('express')
-require('http')
 
 const app = express()
 const port = process.env.PORT || 3000
